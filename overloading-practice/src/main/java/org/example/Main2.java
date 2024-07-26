33742ff65302ac434589d5cccf9473396851a162
FishFlete <fishflete@gmail.com>
2024-07-26 13:52:11 -0400

    Update Main2.java

--- overloading-practice/src/main/java/org/example/Main2.java
+++ overloading-practice/src/main/java/org/example/Main2.java
@@ -10,32 +10,28 @@ public class Main2 {
         ArrayList<Book> books = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        // Adding objects using overloaded constructors
         animals.add(new Animal("Buddy", true));
         animals.add(new Animal("Whiskers", false));
-        animals.add(new Animal());  // Using default constructor
+        animals.add(new Animal()); 
 
         tvShows.add(new TvShow("Breaking Bad", 62, "Drama"));
         tvShows.add(new TvShow("Friends", 236, "Comedy"));
-        tvShows.add(new TvShow());  // Using default constructor
+        tvShows.add(new TvShow());  
 
         books.add(new Book("The Great Gatsby", 180, 1925));
         books.add(new Book("To Kill a Mockingbird", 281, 1960));
-        books.add(new Book());  // Using default constructor
+        books.add(new Book()); 
 
-        // Print all animals
         System.out.println("Animal List:");
         for (Animal animal : animals) {
             System.out.println(animal);
         }
 
-        // Print all TV shows
         System.out.println("\nTV Show List:");
         for (TvShow tvShow : tvShows) {
             System.out.println(tvShow);
         }
 
-        // Print all books
         System.out.println("\nBook List:");
         for (Book book : books) {
             System.out.println(book);
